e3849098444401193ce5c5b6a052083f9d4fdbe9
Acer <natsuaryal13@gmail.com>
2024-09-08 20:06:27 +0545

    Initial commit

---
+++ src/Models/BookOrder.java
@@ -0,0 +1,38 @@
+package Models;
+
+import java.util.List;
+
+public class BookOrder {
+    private List<BookQuantity> bookQuantities;
+    private double bookPrice;
+
+    public BookOrder(List<BookQuantity> bookQuantities, double bookPrice) {
+        this.bookQuantities = bookQuantities;
+        this.bookPrice = bookPrice;
+    }
+
+    public List<BookQuantity> getBookQuantities() {
+        return bookQuantities;
+    }
+
+    public void setBookQuantities(List<BookQuantity> bookQuantities) {
+        this.bookQuantities = bookQuantities;
+    }
+
+    public double getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(double bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+
+    public double calculateTotalPrice() {
+        double total = 0.0;
+        for (BookQuantity bq : bookQuantities) {
+            total = total + bq.getQuantity() * bookPrice;
+        }
+        return total;
+    }
+}
